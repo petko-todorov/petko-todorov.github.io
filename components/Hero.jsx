32a7d461587d26282826c8fce6452b3d0ee42526
Petko Todorov <p.todorov1015@gmail.com>
2026-04-29 13:50:16 +0300

    Update Hero component text and styling

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,22 +11,26 @@ export default function Hero() {
         <>
             <section
                 id="home"
-                className="relative h-screen w-full overflow-hidden text-white"
+                className="relative min-h-screen w-full overflow-hidden text-white"
             >
                 <div className="absolute inset-0 -z-20">
                     <VantaBackground />
                 </div>
 
-                <div className="absolute inset-0 backdrop-blur-[2px] bg-black/20 -z-10 pointer-events-none"></div>
+                <div className="absolute inset-0 backdrop-blur-[2px] -z-10 pointer-events-none"></div>
 
-                <div className="relative z-10 flex flex-col items-center justify-center h-full text-center pb-20">
+                <div className="relative z-10 flex flex-col items-center justify-center min-h-screen text-center pb-20">
                     <div className="space-y-5">
                         <h1
                             data-aos="fade-left"
-                            className="text-3xl font-bold uppercase"
+                            className="text-4xl font-bold uppercase"
                         >
-                            Hi, I am Petko
+                            Hi, I am
+                            <span className="pl-2 text-transparent text-l bg-clip-text bg-linear-to-r from-white to-[#00E5FF]">
+                                PETKO
+                            </span>
                         </h1>
+
                         <h2
                             data-aos="fade-right"
                             className="text-3xl font-bold text-[#00E5FF] uppercase [text-shadow:0_0_2px_#00E5FF,0_0_1px_#00E5FF,0_0_2px_#00E5FF,0_0_1px_rgba(0,229,255,0.5)]"
