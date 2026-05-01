54e794e9f6dad4c9ca8512479a2cc63aa713cd83
Petko Todorov <p.todorov1015@gmail.com>
2026-05-01 18:05:40 +0300

    Add new sections for skills, projects, certificates, and contact

--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
-import AboutMe from '@/components/AboutMe';
+import Certificates from '@/components/Certificates';
+import Contact from '@/components/Contact';
 import Hero from '@/components/Hero';
 import NavBar from '@/components/NavBar';
+import Projects from '@/components/Projects';
+import Skills from '@/components/Skills';
 
 export default function Home() {
     return (
         <>
             <Hero />
             <NavBar />
-            <AboutMe />
+            <Skills />
+            <Projects />
+            <Certificates />
+            <Contact />
         </>
     );
 }
