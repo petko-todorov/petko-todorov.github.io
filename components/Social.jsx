c32633e465fe6f4ba9ff90520ab459b84ba027c4
Petko Todorov <p.todorov1015@gmail.com>
2026-05-01 18:08:23 +0300

    Refactor social links to improve structure

--- components/Social.jsx
+++ components/Social.jsx
@@ -10,53 +10,45 @@ export default function Social() {
     return (
         <div className="pt-14 text-2xl flex justify-center">
             <ul className="flex justify-center space-x-3">
-                <a
-                    href="https://www.facebook.com/petko0/"
-                    target="_blank"
-                    className="hover:text-[#1F91FF] duration-300"
-                >
-                    <div data-aos="zoom-in" data-aos-delay="100">
-                        <li className="border-2 border-[#BCEDFF] w-15 h-12 rounded-xl flex items-center justify-center">
-                            <FontAwesomeIcon icon={faFacebookF} />
-                        </li>
-                    </div>
-                </a>
+                <li data-aos="zoom-in" data-aos-delay="100">
+                    <a
+                        href="https://www.facebook.com/petko0/"
+                        target="_blank"
+                        className="hover:text-[#1F91FF] duration-300 border-2 border-[#BCEDFF] w-15 h-12 rounded-xl flex items-center justify-center"
+                    >
+                        <FontAwesomeIcon icon={faFacebookF} />
+                    </a>
+                </li>
 
-                <a
-                    href="https://github.com/petko-todorov/"
-                    target="_blank"
-                    className="hover:text-[#181717] duration-300"
-                >
-                    <div data-aos="zoom-in" data-aos-delay="300">
-                        <li className="border-2 border-[#BCEDFF] w-15 h-12 rounded-xl flex items-center justify-center">
-                            <FontAwesomeIcon icon={faGithub} />
-                        </li>
-                    </div>
-                </a>
+                <li data-aos="zoom-in" data-aos-delay="300">
+                    <a
+                        href="https://github.com/petko-todorov/"
+                        target="_blank"
+                        className="hover:text-[#181717] duration-300 border-2 border-[#BCEDFF] w-15 h-12 rounded-xl flex items-center justify-center"
+                    >
+                        <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                </li>
 
-                <a
-                    href="https://www.linkedin.com/in/petko0/"
-                    target="_blank"
-                    className="hover:text-[#0A66C2] duration-300"
-                >
-                    <div data-aos="zoom-in" data-aos-delay="500">
-                        <li className="border-2 border-[#BCEDFF] w-15 h-12 rounded-xl flex items-center justify-center">
-                            <FontAwesomeIcon icon={faLinkedinIn} />
-                        </li>
-                    </div>
-                </a>
+                <li data-aos="zoom-in" data-aos-delay="500">
+                    <a
+                        href="https://www.linkedin.com/in/petko0/"
+                        target="_blank"
+                        className="hover:text-[#0A66C2] duration-300 border-2 border-[#BCEDFF] w-15 h-12 rounded-xl flex items-center justify-center"
+                    >
+                        <FontAwesomeIcon icon={faLinkedinIn} />
+                    </a>
+                </li>
 
-                <a
-                    href="https://discordapp.com/users/petko007/"
-                    target="_blank"
-                    className="hover:text-[#5662EC] duration-300"
-                >
-                    <div data-aos="zoom-in" data-aos-delay="700">
-                        <li className="border-2 border-[#BCEDFF] w-15 h-12 rounded-xl flex items-center justify-center">
-                            <FontAwesomeIcon icon={faDiscord} />
-                        </li>
-                    </div>
-                </a>
+                <li data-aos="zoom-in" data-aos-delay="700">
+                    <a
+                        href="https://discordapp.com/users/petko007/"
+                        target="_blank"
+                        className="hover:text-[#5662EC] duration-300 border-2 border-[#BCEDFF] w-15 h-12 rounded-xl flex items-center justify-center"
+                    >
+                        <FontAwesomeIcon icon={faDiscord} />
+                    </a>
+                </li>
             </ul>
         </div>
     );
