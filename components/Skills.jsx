b69346c56fa78f178b08b9d193be513cc9e90471
Petko Todorov <p.todorov1015@gmail.com>
2026-05-01 18:07:59 +0300

    Refactor skill list items to use semantic HTML tags

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -55,7 +55,7 @@ function Skills() {
                 </h1>
 
                 <div className="flex flex-wrap justify-center mx-auto gap-7 text-center text-white">
-                    <div
+                    <article
                         className="flex flex-col bg-[#122131] space-y-6 border-2 border-gray-700 py-8 px-10 rounded-xl md:w-2/5 lg:w-1/4 max-sm:w-2/3"
                         data-aos="fade-right"
                     >
@@ -66,9 +66,9 @@ function Skills() {
                             {backEndSkills.description}
                         </p>
 
-                        <div className="grid grid-cols-3 gap-8">
+                        <ul className="grid grid-cols-3 gap-8">
                             {backEndSkills.skills.map((skill, index) => (
-                                <div
+                                <li
                                     key={index}
                                     className="flex flex-col items-center"
                                 >
@@ -80,12 +80,12 @@ function Skills() {
                                     <span className="text-sm font-semibold">
                                         {skill.name}
                                     </span>
-                                </div>
+                                </li>
                             ))}
-                        </div>
-                    </div>
+                        </ul>
+                    </article>
 
-                    <div
+                    <article
                         className="flex flex-col bg-[#122131] space-y-6 border-2 border-gray-700 py-8 px-10 rounded-xl md:w-2/5 lg:w-1/4 max-sm:w-2/3"
                         data-aos="fade-up"
                     >
@@ -96,9 +96,9 @@ function Skills() {
                             {frontEndSkills.description}
                         </p>
 
-                        <div className="grid grid-cols-3 gap-8">
+                        <ul className="grid grid-cols-3 gap-8">
                             {frontEndSkills.skills.map((skill, index) => (
-                                <div
+                                <li
                                     key={index}
                                     className="flex flex-col items-center"
                                 >
@@ -110,12 +110,12 @@ function Skills() {
                                     <span className="text-sm font-semibold">
                                         {skill.name}
                                     </span>
-                                </div>
+                                </li>
                             ))}
-                        </div>
-                    </div>
+                        </ul>
+                    </article>
 
-                    <div
+                    <article
                         className="flex flex-col bg-[#122131] space-y-6 border-2 border-gray-700 py-8 px-10 rounded-xl md:w-2/5 lg:w-1/4 max-sm:w-2/3"
                         data-aos="fade-left"
                     >
@@ -126,9 +126,9 @@ function Skills() {
                             {tools.description}
                         </p>
 
-                        <div className="grid grid-cols-3 gap-8">
+                        <ul className="grid grid-cols-3 gap-8">
                             {tools.skills.map((skill, index) => (
-                                <div
+                                <li
                                     key={index}
                                     className="flex flex-col items-center"
                                 >
@@ -140,10 +140,10 @@ function Skills() {
                                     <span className="text-sm font-semibold">
                                         {skill.name}
                                     </span>
-                                </div>
+                                </li>
                             ))}
-                        </div>
-                    </div>
+                        </ul>
+                    </article>
                 </div>
 
                 <div className="mb-35"></div>
